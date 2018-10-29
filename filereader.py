267e73b0f59d6d4ac227ab1bf1f52af0b1f0fbb5
AriosJentu <darthpoezd@gmail.com>
2018-10-30 09:54:51 +1000

    Added some methods for FileReader

--- filereader.py
+++ filereader.py
@@ -7,14 +7,87 @@ import json
 class PDBFileClass:
 
 	def __init__(self, filename):
+	
+		#Getting file name and format:
+		file_name = filename[:filename.find(".")]
+		file_format = filename[filename.find("."):]
+
+		cur_dir = ".__"+file_name+"__/"		#Current directory
 		
-		#----------------------------------
-		#-- TODO: Initializate archive ----
-		#-- Get information about tables --
-		#----------------------------------
+		#Check for existing directory
+		if os.path.isdir(cur_dir):
+			warnings.warn("File was opened.")
+
+		else:
+			os.mkdir(cur_dir)				#Making base directory
+
+		
+		self.file = zipfile.ZipFile(filename, "r")
+		self.file.extractall(cur_dir)
+
+		self.filename = file_name+file_format
+		self.current_dir = cur_dir
+		self.files_list = []
+
+		#Push all files inside array
+		for i in self.file.filelist:
+
+			#Check - is item not directory
+			if os.path.isfile(cur_dir+i.filename):
+				self.files_list.append(i.filename)
+
+	def close(self):
+		
+		#Removing current directory
+		shutil.rmtree(self.current_dir, ignore_errors=True)	
+		
+		self.file.close()
+
+	def commit(self):
+		
+		#Close current file
+		self.file.close()
+
+		#And open it in write mode
+		with zipfile.ZipFile(self.filename, "w") as db:
+			for i in self.files_list:
+				db.write(self.current_dir+i, i)
+
+		#And reopen it in read mode
+		self.file = zipfile.ZipFile(self.filename, "r")
+
+	def open_file(self, filename, mode="r"):
+		
+		#--------------------------------------------
+		#-- TODO: Create file in archive directory --
+		#-- Save it's name in array -----------------
+		#-- Return object with FILE class -----------
+		#--------------------------------------------
 
 		pass
 
+	def create_directory(self, dirname):
+
+		#----------------------------
+		#-- TODO: Create directory --
+		#----------------------------
+
+		pass
+
+	def remove_file(self, filename):
+
+		#----------------------------------------------
+		#-- TODO: Remove file from archive directory --
+		#-- Remove it's name from array ---------------
+		#----------------------------------------------
+
+	def remove_directory(self, dirname):
+
+		#----------------------------
+		#-- TODO: Remove directory --
+		#----------------------------
+
+		pass
 
 class DataBase:
 
@@ -23,14 +96,15 @@ class DataBase:
 		#Creating database
 
 		#Checking - is file exists:
-		if os.path.isfile(filename+".jpdb"):
+		if os.path.isfile(filename):
 			warnings.warn("File exist. Rewriting.")
 
-		#Making base directory
-		os.mkdir("__"+filename+"__")
+		#Getting file name and format:
+		file_name = filename[:filename.find(".")]
+		file_format = filename[filename.find("."):]
 
-		#Current directory
-		cur_dir = "__"+filename+"__/"
+		cur_dir = ".__"+file_name+"__/"		#Current directory
+		os.mkdir(cur_dir)					#Making base directory
 
 		#Meta file creation
 		with open(cur_dir+"meta.pkl", "w") as meta:
@@ -39,29 +113,32 @@ class DataBase:
 			json.dump({
 				"name": filename,		#Database name
 				"tables": []			#List of tables in DB
-			}, meta)					#To meta file
+			}, meta)					#To meta file\
+
 
 		#Saving zip file:
-		with zipfile.ZipFile(filename+".jpdb","w") as database:
+		with zipfile.ZipFile(filename, "w") as database:
 			database.write(cur_dir+"meta.pkl", "meta.pkl")
 
-		#Removing current directory
-		shutil.rmtree("__"+filename+"__/", ignore_errors=True)
-
-		#Returning file class
-		return PDBFileClass(filename)
+		shutil.rmtree(cur_dir, ignore_errors=True)	#Removing current directory
+		return PDBFileClass(filename) 				#Returning file class
 
 	@staticmethod
 	def open(filename):
 		#Opening database
 
 		#Checking - is file exists:
-		if not os.path.isfile(filename+".jpdb"):
+		if not os.path.isfile(filename):
+
+			#If not exist - create
 			warnings.warn("File doesn't exist. Creating.")
-			return PDBWorkFile.create(filename)
-
-		#Returning file class
-		return PDBFileClass(filename)
+			return DataBase.create(filename)
 
 
-db = DataBase.open("database")
+		return PDBFileClass(filename)		#Returning file class
+
+DataBase()
+db = DataBase.open("database.jpdb")
+print(db.files_list)
+db.commit()
+db.close()
