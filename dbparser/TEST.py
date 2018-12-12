b7239d4d65005f87abb33591a17f775508e5d730
Enomae <manuelromem@gmail.com>
2018-12-12 20:59:13 +1000

    Update parser

--- dbparser/TEST.py
+++ dbparser/TEST.py
@@ -1,4 +1,7 @@
-import newParser
+try:
+	import dbparser.dbparse as parse
+except:
+	import dbparse as parse
 import re
 
 """
@@ -15,31 +18,22 @@ UPDATE 'table' SET field1=val1, 'field2'='val2' WHERE cond
 #s = '''CREATE TABLE 'test' (V int, 'Value1' str, "Value2" bol, val integer, 'val1' string, "val2" bool)'''
 #s = '''SHOW CREATE TABLE TEST'''
 #s = '''SELECT value, value1 FROM test'''
-s = '''SELECT (value, value1) FROM test WHERE value - -1 AND X OR Y'''
+#s = '''SELECT (value, value1) FROM test WHERE value - -1 AND X OR Y'''
 #s = '''SELECT (value, value1) FROM test WHERE (value - -1 AND X OR Y)'''
 #s = '''SELECT (value, value1) FROM test WHERE (value - -1) AND (X OR Y)'''
 #s = '''INSERT INTO 'test' (val1, val2)'''
-#s = '''INSERT INTO 'test' VALUES(val1, val2)'''
+s = '''INSERT INTO 'test' VALUES (val1, val2)'''
 
 
+tree = parse.build_tree(s)
+
+print(tree[1])
+print(tree[0].type)
+print(tree[0].insert.name)
+print(tree[0].insert.fields)
+print()
 
 
-
-print(newParser.build_tree(s)[0])
-tree = newParser.build_tree(s)[1]
-
-for i in tree:
-	print(i)
-	if type(tree[i]) != str:
-		for j in tree[i]:
-			print("\t", j)
-			if type(tree[i][j]) != str:
-				for k in tree[i][j]:
-					print("\t\t", k)
-			else:
-				print("\t\t", tree[i][j])
-	else:	
-		print("\t", tree[i])
 
 
 
